e1cc4e81338cbc2f573cdee7924dff4718fe36fe
Aleksey-Shulzhenko <160366926+Aleksey-Shulzhenko@users.noreply.github.com>
2025-03-18 23:19:14 +0200

    Add files via upload

---
+++ new-game-project/level.gd
@@ -0,0 +1,56 @@
+extends Node2D
+
+@onready var light = $DirectionalLight2D
+@onready var pointlight = $PointLight2D
+@onready var day_text = $CanvasLayer/DayText
+@onready var animPlayer = $CanvasLayer/AnimationPlayer
+
+enum {
+	MORNING,
+	DAY,
+	EVENING,
+	NIGHT
+}
+
+var state = MORNING
+var day_count: int 
+
+func _ready():
+	light.enabled = true
+	day_count = 1
+	set_day_text()
+	day_text_fade()
+			
+func morning_state ():
+	var tween = get_tree().create_tween()
+	tween.tween_property(light, "energy", 0.2, 20)
+	var tween1 = get_tree().create_tween()
+	tween1.tween_property(pointlight, "energy", 0, 20)
+func evening_state ():
+	var tween = get_tree().create_tween()
+	tween.tween_property(light, "energy", 0.95, 20)
+	var tween1 = get_tree().create_tween()
+	tween1.tween_property(pointlight, "energy", 1.5, 20)
+
+func _on_day_night_timeout() -> void:
+	match state:
+		MORNING:
+			morning_state()
+		EVENING:
+			evening_state()
+	if state < 3:
+		state += 1
+	else:
+		state = MORNING
+		day_count += 1
+		set_day_text()
+		day_text_fade()
+
+
+func day_text_fade():
+	animPlayer.play("day_text_fade_in")
+	await get_tree().create_timer(3).timeout
+	animPlayer.play("day_text_fade_out")
+
+func set_day_text ():
+	day_text.text = "DAY " + str(day_count)
